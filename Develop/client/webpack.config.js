b07064b069df423e82fddf96e4644e04d22b0b14
FIRST_NAME LAST_NAME <safiamustaf>
2023-06-01 05:07:44 -0500

    plugins

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -27,7 +27,24 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J-A-T-E',
+        description: 'Browser based Note taking  application!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
