01dcbdef7a7b79d5e3904e187d5b34e2a7218b30
FIRST_NAME LAST_NAME <safiamustaf>
2023-06-01 05:23:24 -0500

    added content to database in database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,7 +14,13 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) =>{
-
+  console.log('In putDb ');
+  const pwaDb = await openDB('jate', 1);
+  const txn = jateDb.transaction('jate', 'readwrite');
+  const str = txn.objectStore('jate');
+  const req = str.put({ id: 1, value: content });
+  const res = await req;
+  console.log('putDb result =  ', res.value);
  }
 
 // TODO: Add logic for a method that gets all the content from the database
