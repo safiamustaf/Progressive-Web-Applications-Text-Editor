79685f096db43962f157085829d9440247cd460a
FIRST_NAME LAST_NAME <safiamustaf>
2023-06-01 05:08:50 -0500

    added rules

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -48,7 +48,11 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [
+      rules: [ 
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
         
       ],
     },
