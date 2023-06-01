13c7ff6dd1bd94e143c258892838975519ef5314
FIRST_NAME LAST_NAME <safiamustaf>
2023-06-01 05:09:41 -0500

    added bable-loader

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -53,7 +53,17 @@ module.exports = () => {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
-        
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
